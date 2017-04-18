7646134cd73bf72b64aeae00db3daa79bd08bc3e
juan267 <j.gomez267@gmail.com>
2017-04-18 11:46:13 -0500

    fix deprecation warnings for rails 5

--- config/application.rb
+++ config/application.rb
@@ -21,7 +21,7 @@ module Xpenses
     # config.i18n.default_locale = :de
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
-    config.active_record.raise_in_transactional_callbacks = true
+    # config.active_record.raise_in_transactional_callbacks = true
 
     config.generators do |generator|
       generator.view_specs false
