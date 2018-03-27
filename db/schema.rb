25bd6ee56de4552325f04686c83698839d9b9972
jeferbc <jeffe.bernal@gmail.com>
2018-03-27 13:33:47 -0500

    fix vulnerabilities github alert

--- db/schema.rb
+++ db/schema.rb
@@ -1,4 +1,3 @@
-# encoding: UTF-8
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -26,8 +25,7 @@ ActiveRecord::Schema.define(version: 20150907032105) do
     t.decimal  "amount"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.index ["category_id"], name: "index_expenses_on_category_id"
   end
-
-  add_index "expenses", ["category_id"], name: "index_expenses_on_category_id"
 
 end
