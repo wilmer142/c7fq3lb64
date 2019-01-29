16a1d5a2e9d9fdbaea1db3334a2cb892fce301cc
wilmer142 <wilmercollege@gmail.com>
2019-01-28 22:22:13 -0500

    Add filter on expense index

--- app/controllers/expenses_controller.rb
+++ app/controllers/expenses_controller.rb
@@ -1,5 +1,18 @@
 class ExpensesController < ApplicationController
   def index
-     @expenses = Expense.order("date DESC")
+  	if params[:category_id].present? && params[:concept].present?
+	 		@expenses = Expense.where("category_id = ? and concept LIKE ?", params[:category_id], "%#{params[:concept]}%")
+  	elsif params[:concept].present?
+  		@expenses = Expense.where("concept LIKE ?", "%#{params[:concept]}%")
+  	elsif params[:category_id].present?
+  		@expenses = Expense.where("category_id = ?", params[:category_id], )
+  	else
+  		@expenses = Expense.all
+  	end
   end
+
+  private 
+  	def expense_params
+  		params.require(:expense).permit(:category_id, :date, :concept, :amount)
+  	end
 end
